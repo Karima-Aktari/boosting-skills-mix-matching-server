5d54bcb247f8ba1a38a0661e65cc963bfe1edadd
Karima-Aktari <karimaaktari911@gmail.com>
2021-12-18 17:05:15 +0600

    commit admin

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ async function run() {
         console.log('Database connected Successfully');
         const database = client.db('mixMatching');
         const winterCollection = database.collection('winter');
-        const ordersCollections = database.collections('orders');
-        console.log(ordersCollections);
+        const ordersCollection = database.collection('orders');
+        const usersCollection = database.collection('users');
+        console.log(usersCollection);
 
         //POST WinterCollections API
         app.post('/winter', async (req, res) => {
@@ -47,14 +48,103 @@ async function run() {
             const winter = await winterCollection.findOne(query);
             res.json(winter);
         })
+        //DELETE Product
+        app.delete('/deleteWinter/:id', async (req, res) => {
+            console.log(req.params.id);
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await winterCollection.deleteOne(query);
+            res.json(result);
+        })
 
-        // //POST Orders API
-        // app.post('/orders', async (req, res) => {
-        //     const order = req.body;
-        //     const result = await ordersCollections.insertOne(order);
-        //     res.send(result);
+        //POST Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result);
+        })
 
-        // })
+        //GET (make) Orders API
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.json(orders);
+        })
+
+        //Delete Order
+        app.delete('/deleteOrder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const order = await ordersCollection.deleteOne(query);
+            res.json(order);
+        })
+
+        // Update Order
+        app.put("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: "Shipped"
+                },
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        });
+        //Get My Orders
+        app.get('/myOrders/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const order = await ordersCollection.find(query).toArray();
+            res.send(order)
+        })
+        //POST users to database
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        })
+        //Update users API
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            // console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+        // User set Role-Admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+
+            if (email) {
+                const query = { email: email };
+                const user = await usersCollection.findOne(query);
+                let isAdmin = false;
+                if (user) {
+                    if (user.role === 'admin') {
+                        isAdmin = true;
+                    }
+                    res.json({ admin: isAdmin })
+                } else {
+                    res.json({ admin: isAdmin })
+                }
+            } else {
+                res.json({ admin: false })
+            }
+        })
+
     }
     finally {
         //await client.close();
